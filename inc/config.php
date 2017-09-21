d5dca21931bc8706b41c61f66fcd91c7483e997f
Sem Spanhaak <sspanhaa8952@student.landstede.nl>
2017-09-21 10:50:36 +0200

    Init

---
+++ inc/config.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+
+    // define('DBHOST','localhost');
+    // define('DBNAME','');
+    // define('DBUSER','');
+    // define('DBPASS','');
+
+    // //database credentials
+    define('DBHOST','localhost');
+    define('DBNAME','workspace');
+    define('DBUSER','root');
+    define('DBPASS','');
+
+    //$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
