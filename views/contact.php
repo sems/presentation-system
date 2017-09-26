510c7a64a38c97963a3329a0905683e16717552c
thobias horst <thorst0023@student.landstede.nl>
2017-09-26 13:57:53 +0200

    google maps

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,1 @@
-<h4>ContCT</h4>
+<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJc_MFuHbmx0cRutjmMZdaBYI&key=AIzaSyBZk4Ozt3WX4PGWpokvq2hQ-Sgy4HkJDlk" allowfullscreen></iframe>
