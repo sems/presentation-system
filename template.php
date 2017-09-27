0bae0184d914b6f1ccc96a2400c71739529f7814
thobias horst <thorst0023@student.landstede.nl>
2017-09-27 11:01:27 +0200

    live chat weg en login pagina gemaakt

--- template.php
+++ template.php
@@ -9,19 +9,6 @@
         <link rel="stylesheet" href="css/main.min.css">
         <title><?php echo $sectionActive ?></title>
     </head>
-	<!--Start of Tawk.to Script-->
-	<script type="text/javascript">
-		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-		(function(){
-		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-		s1.async=true;
-		s1.src='https://embed.tawk.to/59ca445ac28eca75e46225b6/default';
-		s1.charset='UTF-8';
-		s1.setAttribute('crossorigin','*');
-		s0.parentNode.insertBefore(s1,s0);
-		})();
-	</script>
-	<!--End of Tawk.to Script-->
     <body>
         <nav>
             <ul>
--- views/login.php
+++ views/login.php
@@ -1,14 +1,12 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="/css/login.css"/>
 </head>
-<div class="container">
-	<div class="login-page">
-	  <div class="form">
-		<form class="login-form">
-		  <input type="text" placeholder="username"/>
-		  <input type="password" placeholder="password"/>
-		  <button>login</button>
-		</form>
-	  </div>
-	</div>
+<div class="login-page">
+  <div class="form">
+	<form class="login-form">
+	  <input type="text" placeholder="username"/>
+	  <input type="password" placeholder="password"/>
+	  <button>login</button>
+	</form>
+  </div>
 </div>
