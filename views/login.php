f1daa19d4278324dc44e057df9963a666d31a8a7
thobias horst <thorst0023@student.landstede.nl>
2017-09-27 11:01:50 +0200

    scss inplaats van css

--- views/login.php
+++ views/login.php
@@ -1,5 +1,5 @@
 <head>
-	<link rel="stylesheet" type="text/css" href="/css/login.css"/>
+	<link rel="stylesheet" type="text/css" href="/css/login.scss"/>
 </head>
 <div class="login-page">
   <div class="form">
