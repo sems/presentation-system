fdc74290fe0a0fea2c6e249e8df90d8f3953f7b8
Sem Spanhaak <sspanhaa8952@student.landstede.nl>
2017-09-28 09:55:22 +0200

    Paralex added to the prices page

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <div class="content-footer">
+        <ul class="footer-links">
+            <li class="footer-link"><a href="/contact">Contact</a> </li>
+            <li class="footer-link"><a class="footer-social-link" target="_blank" href="">Google</a> </li>
+            <li class="footer-link"><a class="footer-social-link" target="_blank" href="/login">Login</a> </li>
+        </ul>
+    </div>
+</div>
--- views/prices.php
+++ views/prices.php
@@ -1,3 +1,4 @@
+<div class="parallax-window" data-parallax="scroll" data-image-src="img/tof.jpg"></div>
 <section id="pricePlans">
 	<ul id="plans">
         <?php
@@ -108,4 +109,7 @@
 			</div>
 		</form>
 	</div>
+</div>
+<div class="container">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </div>
