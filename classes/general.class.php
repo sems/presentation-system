376708114ebf1fd7caaf12da7f9f55f8a4cd70c7
Sem Spanhaak <sspanhaa8952@student.landstede.nl>
2017-10-04 14:12:30 +0200

    push for thobias

--- classes/general.class.php
+++ classes/general.class.php
@@ -5,7 +5,7 @@
     class General
     {
         // Base function for posting
-        public function postApi($data, $job)
+        private function postApi($data, $job)
         {
             // NOTE: Het format van $data is te vinden bij een subtask
             // Graag ook houden aan de indentatie en inhoud anders kan er een fout ontstaan.
@@ -19,7 +19,7 @@
 
             // Test if the url is correct
             //echo $baseUrl.$job;
-            
+
             curl_setopt($ch, CURLOPT_URL, $baseUrl.$job);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
             curl_setopt($ch, CURLOPT_POST, 1);
@@ -29,8 +29,8 @@
             $response  = curl_exec($ch);
 
             curl_close($ch);
-            //return $response;
-            echo $response;
+            return $response;
+            //echo $response;
         }
 
         public function accountAanmaken($data) {
@@ -52,8 +52,31 @@
                     'Password' => $_POST['l_password']
                 );
             */
-            General::postApi($data, "/api/account/login");
-            return $response;
+            $loginResponse = General::postApi($data, "/api/account/login");
+            if ($loginResponse === "false") {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        private function deleteApi($job) {
+
+            $baseUrl = "http://localhost:63502";
+
+            $ch = curl_init($baseUrl.$job);
+            curl_setopt($ch, CURLOPT_URL, $baseUrl.$job);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response  = curl_exec($ch);
+            curl_close($ch);
+        }
+
+        public function deleteAccount($dataId)
+        {
+            // Call with General::deleteID("3");
+            $job = "/api/Account/".$dataId;
+            $this->deleteApi($job);
         }
     }
 
