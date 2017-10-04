6d93d6ff11aa9f11aa5af330883b034c83dce10f
thobias horst <thorst0023@student.landstede.nl>
2017-10-04 14:11:50 +0200

    login werkend

--- logincheck.php
+++ logincheck.php
@@ -5,15 +5,36 @@ if(!isset($_SESSION['logged_in'])) {
   $_SESSION['logged_in'] = false;
 }
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $l_data = array(
-        'Email' => $_POST['l_email'],
-        'Password' => $_POST['l_password']
-    );
-    if(General::accountLogin($l_data) == true) {
-      $_SESSION['logged_in'] = true;
-      header('Location: index.php');
+  // start chapta
+  
+  if(isset($_POST['g-recaptcha-response'])){
+      // If it is responding set a variable
+      $captcha= $_POST['g-recaptcha-response'];
+  }
+    // Key got from the google account
+    $secretKey = "6LdpFzMUAAAAAG5Qcsqtebtvioo1NCmu2IdXvMbn";
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $cresponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+    $responseKeys = json_decode($cresponse,true);
+
+    if(intval($responseKeys["success"]) !== 1) {
+
+        // No succes
+        header('location: login.php?error=captcha');
     } else {
-      $_SESSION['logged_in'] = false;
-      header('Location: login.php?error');
+
+      $l_data = array(
+          'Email' => $_POST['l_email'],
+          'Password' => $_POST['l_password']
+      );
+      $l = General::accountLogin($l_data);
+
+      if($l == true) {
+        $_SESSION['logged_in'] = true;
+        header('Location: index.php');
+      } else {
+        $_SESSION['logged_in'] = false;
+        header('Location: login.php?error=gegevens');
+      }
     }
 }
