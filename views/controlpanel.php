26298802d5e56e3bcef603916751b1b9c76ff29a
thobias horst <thorst0023@student.landstede.nl>
2017-10-09 13:13:52 +0200

    cp gefixed

--- views/controlpanel.php
+++ views/controlpanel.php
@@ -1,13 +1,14 @@
 <head>
 <link rel="stylesheet" type="text/css" href="/css/cp.css">
+<script src="js/cpmenu.js"></script>
 </head>
 <div id="mySidenav" class="sidenav">
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
+  <a href="#">account gegevens</a>
+  <a href="#">Presentaties</a>
+  <a href="#">beheer gebruikers</a>
+  <a href="#">beheer schermen</a>
 </div>
-<span onclick="openNav()">open</span>
+
 
 <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
 <div class="container">
