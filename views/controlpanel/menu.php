71d62932e1345b7dae859275afeadd47b619a1e4
thobias horst <thorst0023@student.landstede.nl>
2017-10-11 14:30:27 +0200

    menu voor cp

---
+++ views/controlpanel/menu.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" type="text/css" href="/css/cp.css">
+<div id="mySidenav" class="sidenav">
+  <a href="accountgegevens.php">account gegevens</a>
+  <a href="presentatie.php">Presentaties</a>
+  <a href="beheergebruikers.php">beheer gebruikers</a>
+  <a href="beheerschermen.php">beheer schermen</a>
+</div>
