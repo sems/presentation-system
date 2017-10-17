318eded6b97b30fffbcc54f7903aee029ff5866e
thobias horst <thorst0023@student.landstede.nl>
2017-10-17 15:30:24 +0200

    form

--- views/controlpanel/presentation.php
+++ views/controlpanel/presentation.php
@@ -1,4 +1,15 @@
 <?php require_once 'menu.php'; ?>
 <div class="c_container">
-  <p>presentaties</p>
+  <form class="frameCreate">
+    <label for="title">Title</label>
+    <input type="text" name="title" value="">
+    <label for="order">Order index(place in presentation)</label>
+    <input type="text" name="order" value="">
+    <label for="duration">Duration (sec)</label>
+    <input type="text" name="duration" value="">
+    <label for="media">Video(url)</label>
+    <input type="text" name="media" value="">
+    <label for="text">Disciption</label>
+    <textarea name="text" rows="8" cols="80"></textarea>
+  </form>
 </div>
