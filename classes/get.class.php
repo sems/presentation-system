44d35b034ef820fd43fed299360b8c1581441162
Sem Spanhaak <sspanhaa8952@student.landstede.nl>
2017-10-17 15:31:32 +0200

    Make overview for the users

--- classes/get.class.php
+++ classes/get.class.php
@@ -4,7 +4,8 @@ require_once "classes/general.class.php";
 class Get extends General
 {
     public function getUsers() {
-        General::getApi("/api/Account");
+        $response = General::getApi("/api/Account");
+        return $response;
     }
     public function getFrame() {
         $frameresponse = General::getApi("/api/Frame");
