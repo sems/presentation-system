4e3cf4bcbac96e820a7a5e48f3691ae8a21c294e
thobias horst <thorst0023@student.landstede.nl>
2017-11-01 15:20:55 +0100

    renames

--- views/controlpanel/menu.php
+++ views/controlpanel/menu.php
@@ -3,5 +3,5 @@
   <a href="frames.php">Frames</a>
   <a href="controlusers.php">Gebruikers</a>
   <a href="controlscreens.php">Schermen</a>
-  <a href="presentation1.php">Presentation</a>
+  <a href="presentation.php">Presentation</a>
 </div>
