72de8deae1477c26e76d717793e2f30ee705947d
Sem Spanhaak <semspanhaak@gmail.com>
2017-11-28 14:39:29 +0100

    Debug

--- inc/package.inc.php
+++ inc/package.inc.php
@@ -4,5 +4,12 @@
         session_unset();     // unset $_SESSION variable for the run-time
         session_destroy();   // destroy session data in storage
     }
+    function debug_to_console( $data ) {
+        $output = $data;
+        if ( is_array( $output ) )
+            $output = implode( ',', $output);
+
+        echo "<script>console.log( '%c Debug Objects: ".$output ."', 'background: #F00; color: #FF0' );</script>";
+    }
     $template 	= "template.php";
 ?>
