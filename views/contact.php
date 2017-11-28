25a285a59a1d8f764bf4bea27723e0f37a1b7973
thobias horst <thorst0023@student.landstede.nl>
2017-11-28 14:41:03 +0100

    plaatjes updates

--- views/contact.php
+++ views/contact.php
@@ -3,13 +3,13 @@
 </head>
 <iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJc_MFuHbmx0cRutjmMZdaBYI&key=AIzaSyBZk4Ozt3WX4PGWpokvq2hQ-Sgy4HkJDlk" allowfullscreen></iframe>
 <div class="container">
-	<img class="logo" src="/img/medium.png" alt="Logo">
+	<img class="logo" src="img/tof.jpg" alt="Logo">
 	<h1>Contact</h1>
-	<p>Naam: Knoop</p>
-	<p>Adress: Zwolsestraat 63a</p>
-	<p>Postcode: 8100 AE</p>
-	<p>Plaats: Raalte</p>
-	<p>Email: Knoop@knoopinfo.com</p>
-	<p>Telefoon: 0272-426236</p>
-	<p>Fax: 0272-426236</p>
+	<p>Naam: Daxis</p>
+	<p>Adress: Maagdenburgstraat 22</p>
+	<p>Postcode: 7421 ZC</p>
+	<p>Plaats: Deventer</p>
+	<p>Email: info@daxis.nl</p>
+	<p>Telefoon: 0570-633242</p>
+	<p>Fax: 0570-637062</p>
 </div>
