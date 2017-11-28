b35f02f85a5ef7deabaf7c5dbb917293cf895cc1
thobias horst <thorst0023@student.landstede.nl>
2017-11-28 14:42:58 +0100

    daxis op google maps

--- views/contact.php
+++ views/contact.php
@@ -1,7 +1,7 @@
 <head>
 <link rel="stylesheet" type="text/css" href="/css/contact.css">
 </head>
-<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJc_MFuHbmx0cRutjmMZdaBYI&key=AIzaSyBZk4Ozt3WX4PGWpokvq2hQ-Sgy4HkJDlk" allowfullscreen></iframe>
+<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.254036856983!2d6.216683116049892!3d52.23876927976189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7eb7614a29951%3A0xb8c58ce67d280134!2sDaxis+Computer+Systems+BV!5e0!3m2!1snl!2snl!4v1511876510281" allowfullscreen></iframe>
 <div class="container">
 	<img class="logo" src="img/tof.jpg" alt="Logo">
 	<h1>Contact</h1>
