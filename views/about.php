7479b6941729dfea6031c5abc5743dab138f01df
thobias horst <thorst0023@student.landstede.nl>
2017-11-29 09:21:57 +0100

    content

--- views/about.php
+++ views/about.php
@@ -1,3 +1,9 @@
 <div class="container">
-	<p>About</p>
+	<p>Al 30 jaar is Daxis de betrouwbare partner voor onderwijsinstellingen en het bedrijfsleven met de volgende highlights:</P>
+<p>-Beheer - Het Daxis team beheert systemen op locatie of in de Cloud, op basis van de laatste technieken zoals Windows 2016 en Windows 10;</P>
+<p>-Datacenter – Daxis heeft een eigen datacenter voor (virtuele)serverhuur, webhosting, ip back-up;</P>
+<p>-Ip camerabeveiliging – Met ons eigen ontwikkelde Sequrebox applicatie haalt u de laatste beveiligingstechniek in huis. Lokale en/of datacenter opslag van de beelden, automatische doorkoppeling naar de meldkamer, opvolging door een beveiligingsbedrijf - het is allemaal mogelijk;</P>
+<p>-Wifi – Daxis levert Ruckus en Engenius wireless producten voor professionele oplossingen met centrale management, roaming en beveiligde toegang.
+<p>-Didax Zapper – de universele virtuele desktop die op elk besturingssysteem te gebruiken is, waardoor de gebruiker op elk device zijn eigen scherm kan gebruiken. Het beheer is kinderlijk eenvoudig geworden; het universele scherm kan met een druk op de knop gedeployed worden.</P>
+<p>-Didax Drive – de Cloud gebaseerde app van Daxis waarin u alle documenten, foto’s en films kunt plaatsen die u nodig hebt voor de uitoefening van uw werk. Synchronisatie van de bestanden op uw pc of laptop gebeurt automatisch, waardoor u altijd in de cloud een back-up hebt. Didax Drive heeft een beheermodule waarmee de omgeving naar uw eigen idee ingericht kan worden.</p>
 </div>
